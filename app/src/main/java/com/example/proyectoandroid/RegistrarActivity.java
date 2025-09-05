ce31f93e29be2767418111a98894d70a73d1a7dd
Beymar12Amaru <b.amaru@alumnos.santotomas.cl>
2025-09-05 13:20:41 -0400

    probando new repository

--- app/src/main/java/com/example/proyectoandroid/RegistrarActivity.java
+++ app/src/main/java/com/example/proyectoandroid/RegistrarActivity.java
@@ -48,6 +48,7 @@ public class RegistrarActivity extends AppCompatActivity {
 
 
 
+
         btnVolver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
